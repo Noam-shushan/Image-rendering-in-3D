784f73cf775dc72ec0c9f8afcf4f496a11cc81a0
Noam shushan <noam8shu@gmail.com>
2021-04-22 21:47:26 +0300

    final commit ex' 4

--- tests/elements/CameraRayIntersectionsIntegrationTests.java
+++ tests/elements/CameraRayIntersectionsIntegrationTests.java
@@ -97,7 +97,6 @@ public class CameraRayIntersectionsIntegrationTests {
                 }
             }
         }
-
         assertEquals(expected, count, "The number of intersections found was incorrect");
     }
 }
